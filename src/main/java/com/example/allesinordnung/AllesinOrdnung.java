45b13964678d09db95a1c116896c485964327269
Sinan99 <80930045+Sinan99@users.noreply.github.com>
2024-01-05 22:50:35 +0100

    fenster fixiert

--- src/main/java/com/example/allesinordnung/AllesinOrdnung.java
+++ src/main/java/com/example/allesinordnung/AllesinOrdnung.java
@@ -15,7 +15,14 @@ public class AllesinOrdnung extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AllesinOrdnung.class.getResource("Homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        // Festlegen der Größe des Fensters
         stage.setScene(scene);
+        stage.setWidth(900); // Festlegen der Breite
+        stage.setHeight(600); // Festlegen der Höhe
+
+        // Verhindern, dass Benutzer die Größe des Fensters ändern können
+        stage.setResizable(false);
+
         stage.show();
     }
     public static void main(String[] args) {
