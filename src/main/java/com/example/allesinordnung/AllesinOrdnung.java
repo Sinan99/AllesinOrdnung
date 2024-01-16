782972551f47d94aba71b3dff306f66b9a852173
iamwahida <nemati.w@yahoo.com>
2024-01-16 14:25:50 +0100

    formatierung

--- src/main/java/com/example/allesinordnung/AllesinOrdnung.java
+++ src/main/java/com/example/allesinordnung/AllesinOrdnung.java
@@ -22,6 +22,7 @@ public class AllesinOrdnung extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/com/example/allesinordnung/Music.java
+++ src/main/java/com/example/allesinordnung/Music.java
@@ -16,11 +16,13 @@ public class Music {
                  @JsonProperty("songDate") String songDate,
                  @JsonProperty("rating") Double rating,
                  @JsonProperty("comment") String comment) {
+
         this.artistName = artistName;
         this.songTitle = songTitle;
         this.songDate = songDate;
         this.rating = rating;
         this.comment = comment;
+
     }
 
     public String getArtistName() {
